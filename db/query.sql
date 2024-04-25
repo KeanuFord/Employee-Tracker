1d9f1166c0b8406aefc0b71291834f8e79c5f00d
KeanuFord <KeanuF2017@gmail.com>
2024-04-25 01:54:15 -0700

    start function and view employees
    app now starts and views employees

--- db/query.sql
+++ db/query.sql
@@ -4,6 +4,11 @@ INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES
     ('Tom', 'Holland', 2, null);
 
 
-SELECT *  FROM employees
-JOIN roles ON employees.role_id = roles.id
-JOIN departments ON roles.department_id = departments.id;
+SELECT employee.id, employee.first_name, employee.last_name, title, salary, department.name AS department, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+FROM employees employee
+JOIN role
+ON role_id = role.id
+JOIN department
+ON department_id = department.id
+LEFT JOIN employees manager
+manager.id = employee.manager_id;
--- index.js
+++ index.js
@@ -16,3 +16,71 @@ const pool = new Pool(
 )
 pool.connect();
 
+function start() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'action',
+                message: 'What would you like to do?',
+                choices: [
+                    'View all employees',
+                    'View all departments',
+                    'View all roles',
+                    'Add an employee',
+                    'Add a department',
+                    'Add a role',
+                    'Update an employee role',
+                    'Exit'
+                ]
+            }
+        ])
+        .then((answer) => {
+            switch (answer.action) {
+                case 'View all employees':
+                    viewEmployees();
+                    break;
+                case 'View all departments':
+                    viewDepartments();
+                    break;
+                case 'View all roles':
+                    viewRoles();
+                    break;
+                case 'Add an employee':
+                    addEmployee();
+                    break;
+                case 'Add a department':
+                    addDepartment();
+                    break;
+                case 'Add a role':
+                    addRole();
+                    break;
+                case 'Update an employee role':
+                    updateEmployeeRole();
+                    break;
+                case 'Exit':
+                    pool.end();
+                    break;
+            }
+        });
+}
+
+start();
+
+function viewEmployees() {
+    pool.query(
+      `SELECT employee.id, employee.first_name, employee.last_name, title, salary, department.name AS department, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+      FROM employees employee
+      JOIN roles role
+      ON role_id = role.id
+      JOIN departments department
+      ON department_id = department.id
+      LEFT JOIN employees manager
+      ON manager.id = employee.manager_id;`,
+        (err, res) => {
+            if (err) throw err;
+            console.table(res.rows);
+            start();
+        }
+    );
+}
